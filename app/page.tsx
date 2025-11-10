a13ee4658a956c89740a1005a5ddc7bd4f15ff6e
Kelia Simbi <simbikelia@gmail.com>
2025-11-10 21:19:14 +0200

    Import button component from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <p>Mento is a notion clone</p>
+      <Button variant={"destructive"}>
+        Click Me!
+      </Button>
     </div>
   );
 }
