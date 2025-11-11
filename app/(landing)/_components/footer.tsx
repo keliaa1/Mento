9785fda9268305b3da15a59cc4bcde367a101494
Kelia Simbi <simbikelia@gmail.com>
2025-11-11 07:12:32 +0200

    Add logo navbar

---
+++ app/(landing)/_components/footer.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Logo from "./logo";
+const Footer = () => {
+    return ( 
+        <div className="flex items-center w-full p-6 bg-background z-50">
+            <Logo />
+
+            {/* push here */}
+
+           
+        </div>
+
+     );
+}
+ 
+export default Footer;
