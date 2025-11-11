2fe243b9d9cc69c0aa8a50c07d7ad6d85b138a52
Kelia Simbi <simbikelia@gmail.com>
2025-11-11 07:24:24 +0200

    Add trunk and import the new componenents

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,6 +1,6 @@
-import Heading from "../_components/heading";
-import Heroes from "../_components/heroes";
-import Footer from "../_components/footer";
+import Heading from "./_components/heading";
+import Heroes from "./_components/heroes";
+import Footer from "./_components/footer";
 const landingPage = () => {
     return ( 
 
