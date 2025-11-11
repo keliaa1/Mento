37e677a3fd4a0cb1575c1c7e464fafa0d0b29a05
Kelia Simbi <simbikelia@gmail.com>
2025-11-11 07:17:16 +0200

    Create logo component

---
+++ app/(landing)/_components/logo.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+import {cn} from "@/lib/utils"
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["400", "600"]
+});
+const  Logo= () => {
+    return ( 
+<div className="hidden md:flex items-center gap-x-2">
+    <Image
+    src="/logo-black.png"
+    height="40"
+    width="40"
+    alt="Mento"
+    />
+    <p className={cn("font-semibold", font.className)}>Mento</p>
+
+</div>
+     );
+}
+ 
+export default Logo;
