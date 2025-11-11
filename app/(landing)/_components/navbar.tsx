6524e7c5b417c90741cbd2fb341434bc0807dceb
Kelia Simbi <simbikelia@gmail.com>
2025-11-11 07:20:24 +0200

    Create navbar component and navbar link

--- app/(landing)/_components/navbar.tsx
+++ app/(landing)/_components/navbar.tsx
@@ -12,11 +12,12 @@ const Navbar = () => {
         <div className={cn(
             "z-50 bg-background fixed top-0 flex items-center w-full p-6", scrolled && "border-b shadow-sm"
         )}>
-
-         
+            <Logo />
+            <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                Login
+            </div>
         </div>
 
-        // Push to github too create navbar Component
      );
 }
  
