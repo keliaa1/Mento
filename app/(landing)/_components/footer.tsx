593aa55957b774041d42beee2b822b5a48a0dff6
Kelia Simbi <simbikelia@gmail.com>
2025-11-11 07:13:19 +0200

    Add footer links

--- app/(landing)/_components/footer.tsx
+++ app/(landing)/_components/footer.tsx
@@ -5,9 +5,15 @@ const Footer = () => {
         <div className="flex items-center w-full p-6 bg-background z-50">
             <Logo />
 
-            {/* push here */}
+            <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
+                <Button variant="ghost" size="sm">
+                    Privacy policy
+                </Button>
+                <Button variant="ghost" size="sm">
+                    Terms and conditions
+                </Button>
 
-           
+            </div>
         </div>
 
      );
