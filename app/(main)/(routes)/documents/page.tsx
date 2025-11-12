a0ac548314dccd96a743b14ff90b7212fdbfcadb
Kelia Simbi <simbikelia@gmail.com>
2025-11-12 23:26:59 +0200

    Add thinking images and display user name in editing section

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,6 +1,29 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
 const documents = () => {
+    const {user}= useUser();
     return (
-        <div>This is the documents page</div>
+        <div className="h-full flex flex-col items-center justify-center">
+
+            <Image
+            src="/thinking.png"
+            height="300"
+            width="300"
+            alt="Mento"
+            className="dark:hidden"
+            />
+            <Image
+            src="/thinking-white.png"
+            height="300"
+            width="300"
+            alt="Mento"
+            className="hidden dark:block"
+            />
+            <h2 className="text-lg font-medium">
+                Welcome to {user?.firstName}&apos;s Mento!
+            </h2>
+        </div>
      );
 }
 
