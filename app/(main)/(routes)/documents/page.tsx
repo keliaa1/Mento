592e2244b5265fdb72bc9cbc22a810e9e59bdc81
Kelia Simbi <simbikelia@gmail.com>
2025-11-14 07:47:16 +0200

    Implement creating post api

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -3,8 +3,23 @@ import { useUser } from "@clerk/nextjs";
 import { PlusCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 const documents = () => {
     const {user}= useUser();
+    const create = useMutation(api.document.create);
+    const onCreate = async () => {
+        const promise = create({title:"Untitled"});
+
+        toast.promise(promise, {
+            loading:"creating...",
+            success:"created!",
+            error:"something went wrong"
+        })
+    }
+
+
     return (
         <div className="h-full flex flex-col items-center justify-center">
 
@@ -25,7 +40,7 @@ const documents = () => {
             <h2 className="text-lg font-medium mb-2">
                 Welcome to {user?.firstName}&apos;s Mento!
             </h2>
-            <Button>
+            <Button onClick={onCreate}>
                 <PlusCircle className="h-4 w-4 ml-2" />
                Create something Mento!
             </Button>
