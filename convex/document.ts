93109b3928c4858e886cafd5a8c6d5095da648ba
Kelia Simbi <simbikelia@gmail.com>
2025-11-18 00:42:52 +0200

    Initialize archive functionality in doument.ts

--- convex/document.ts
+++ convex/document.ts
@@ -3,6 +3,35 @@ import {v} from "convex/values";
 import {mutation, query} from "./_generated/server";
 import {Doc, Id} from "./_generated/dataModel";
 
+export const archive = mutation({
+    args: {
+        id: v.id("documents"),
+    },
+    handler: async (ctx, args) => {
+         const identity = await ctx.auth.getUserIdentity();
+        if(!identity){
+            throw new Error("Not authenticated!");
+        }
+        const userId = identity.subject;
+
+        const existingDocument = await ctx.db.get(args.id);
+
+        if(!existingDocument){
+            throw new Error("Document not found");
+        }
+
+        if(existingDocument.userId !== userId){
+            throw new Error("Unauthorized");
+        }
+
+        const document = await ctx.db.patch(args.id, {
+            isArchived: true
+        });
+
+        return document;
+    }
+})
+
 export const getSidebar = query({
     args: {
         parentDocument: v.optional(v.id("documents"))
