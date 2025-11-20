b4e36033e3df90c0c697faf710912906662c28c4
Kelia Simbi <simbikelia@gmail.com>
2025-11-20 21:08:27 +0200

    Define dialog box preventing deletion

--- components/modals/confirm-modal.tsx
+++ components/modals/confirm-modal.tsx
@@ -1,16 +1,39 @@
-import{
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-}from "@/components/ui/alert-dialog"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
-interface ConfirmModalProps{
-    children: React.ReactNode
-    onConfirm: () => void
+interface ConfirmModalProps {
+  children: React.ReactNode;
+  onConfirm: () => void;
+}
+export const ConfirmModal = ({ children, onConfirm }: ConfirmModalProps) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger onClick={(e) => e.stopPropagation()} asChild>
+        {children}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Do you purpose to delete this?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone typeshii!
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={(e) => e.stopPropagation}>
+            Nvm, gng
+          </AlertDialogCancel>
+          <AlertDialogAction>Yh Im sure gng</AlertDialogAction>
+        </AlertDialogFooter>
+        </AlertDialogContent>
+    </AlertDialog>
+  );
 };
