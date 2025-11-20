1d9a43b20ad64816f722acdea5c69a529e1c8891
Kelia Simbi <simbikelia@gmail.com>
2025-11-21 00:03:41 +0200

    Initialize and create Search-command

---
+++ components/search-command.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { File } from "lucide-react";
+import { useQuery } from "convex/react";
+import { useRouter } from "next/navigation";
+import { useUser } from "@clerk/nextjs";
+
+import{
+    CommandDialog,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+}from "@/components/ui/command";
+import { useSearch } from "@/Hooks/use-search";
+import { api } from "@/convex/_generated/api";
