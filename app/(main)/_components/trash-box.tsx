a900f2aff0640f6cab52403bd46bab0531339585
Kelia Simbi <simbikelia@gmail.com>
2025-11-20 20:05:06 +0200

    use input component and frontend for filtering documents

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -7,7 +7,8 @@ import { useState } from "react";
 import { Id } from "@/convex/_generated/dataModel";
 import { toast } from "sonner";
 import { Spinner } from "@/components/spinner";
-
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
 const TrashBox = () => {
     const router = useRouter();
     const params = useParams();
@@ -63,8 +64,20 @@ const TrashBox = () => {
     }
 
     return (
-        <div>
-            TrashBox
+        <div className="text-sm">
+            <div className="flex items-center gap-x-1 p-2">
+                <Search className="h-4 w-4" />
+                <Input
+                value={search}
+                onChange={(e)=>setSearch(e.target.value)}
+                className="h-7 px-2 focus-visible:ring-transparent bg-secondary"
+                placeholder="Filter by page title..."
+                />
+            </div>
+            <div className="mt-2 px-1 pb-1">
+                <p className="hidden last:block text-xs text-center text-muted-foreground">No documents found</p>
+            </div>
+
         </div>
      );
 }
