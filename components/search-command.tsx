623c60d1f7a215d4c3bd0c80c176004d12ef4386
Kelia Simbi <simbikelia@gmail.com>
2025-11-21 07:45:49 +0200

    Create command dialog for searching

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -32,4 +32,41 @@ export const SearchCommand = ()=>{
 
     }, []);
 
+    const onSelect = (id:string)=>{
+        router.push(`/document/${id}`);
+        onClose();
+    }
+
+    if(!isMounted){
+        return null;
+    }
+
+    return(
+        <CommandDialog open={isOpen} onOpenChange={onClose}>
+            <CommandInput placeholder={`Search ${user?.fullName} 's Mento..`} />
+            <CommandList>
+                <CommandEmpty>Nah u trippin typeshiii, nothing found</CommandEmpty>
+                <CommandGroup heading="Documents">
+                    {documents?.map((document)=>(
+                        <CommandItem
+                        key={document._id}
+                        value={`${document._id}-${document.title}`}
+                        title={document.title}
+                        onSelect={()=>{}}
+                        >
+                            {document.icon ? (
+                                <p className="mr-2 text-[18px]">
+                                    {document.icon}
+                                </p>
+                            ):(
+                                <File className="mr-2 h-4 w-4" />
+                            )}
+
+                        </CommandItem>
+                    ))}
+                </CommandGroup>
+            </CommandList>
+        </CommandDialog>
+    )
 }
+
