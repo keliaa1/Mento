143c939270cc171365eedd295add9c5e2ea4fd23
Kelia Simbi <simbikelia@gmail.com>
2025-11-21 07:55:16 +0200

    Implement keyboard shortcut to search in layout

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -64,7 +64,7 @@ export const SearchCommand = ()=>{
                         key={document._id}
                         value={`${document._id}-${document.title}`}
                         title={document.title}
-                        onSelect={()=>{}}
+                        onSelect={onSelect}
                         >
                             {document.icon ? (
                                 <p className="mr-2 text-[18px]">
@@ -73,6 +73,7 @@ export const SearchCommand = ()=>{
                             ):(
                                 <File className="mr-2 h-4 w-4" />
                             )}
+                            <span>{document.title}</span>
 
                         </CommandItem>
                     ))}
