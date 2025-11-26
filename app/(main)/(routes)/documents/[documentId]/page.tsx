22aa4ca3bda94c53fa2a0ab972eb167a55c10078
Kelia Simbi <simbikelia@gmail.com>
2025-11-26 21:47:33 +0200

    Refactor Toolbar component: move to a new location and enhance functionality with IconPicker integration

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,32 +1,32 @@
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import Toolbar from "@/app/(main)/_components/toolbar";
+import Toolbar from "@/components/toolbar";
 
 interface DocumentIdPageProps {
-    params:{
-        documentId: Id<"documents">;
-    }
+  params: {
+    documentId: Id<"documents">;
+  };
 }
 
-const DocumentIdPage = ({params}:DocumentIdPageProps) => {
-    const document = useQuery(api.document.getById, {
-        documentId: params.documentId,
-    });
-    if(document === undefined){
-        return <div>Loading...</div>;
-    }
-    if(document === null){
-        return <div>Document not found</div>;
-    }
-    return (
-        <div className="pb-40">
-            <div className="h-[35vh]" />
-            <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-            <Toolbar initialData={document} />
-            </div>
-        </div>
-     );
-}
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.document.getById, {
+    documentId: params.documentId,
+  });
+  if (document === undefined) {
+    return <div>Loading...</div>;
+  }
+  if (document === null) {
+    return <div>Document not found</div>;
+  }
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
+};
 
 export default DocumentIdPage;
