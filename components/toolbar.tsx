3c8a08bccc8fda332a15906499c6b015089a7618
Kelia Simbi <simbikelia@gmail.com>
2025-11-28 19:43:35 +0200

    Enhance Toolbar component: integrate IconPicker for icon selection and add functionality to remove icons

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -7,6 +7,7 @@ import { IconPicker } from "./icon-picker";
 import { Button } from "@/components/ui/button";
 import { Divide, Icon, ImageIcon, Smile, X } from "lucide-react";
 import { useMutation } from "convex/react";
+import { on } from "events";
 interface ToolbarProps {
   initialData: Doc<"documents">;
   preview?: boolean;
@@ -18,6 +19,7 @@ const Toolbar = ({ initialData, preview }: ToolbarProps) => {
   const [value, setValue] = useState(initialData.title);
 
   const update = useMutation(api.document.update);
+  const removeIcon = useMutation(api.document.removeIcon);
 
   const enableInput=()=>{
     if(preview) return;
@@ -42,17 +44,29 @@ const Toolbar = ({ initialData, preview }: ToolbarProps) => {
       disableInput();
     }};
 
+    const onIconSelect = (icon:string)=>{
+      update({
+        id:initialData._id,
+        icon,
+      });
+    };
+const onRemoveIcon = ()=>{
+  removeIcon({
+    id: initialData._id,
+  });
+}
+
   return (
     <div className="pl-[54px] group relative">
       {!!initialData.icon && !preview && (
         <div className="flex items-center gap-x-2 group/icon pt-6">
-          <IconPicker onChange={() => {}}>
+          <IconPicker onChange={onIconSelect} >
             <p className="text-6xl hover:opacity-75 transition">
               {initialData.icon}
             </p>
           </IconPicker>
           <Button
-          onClick={()=>{}}
+          onClick={onRemoveIcon}
           className="rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs"
           variant="outline"
           size="icon"
@@ -71,7 +85,7 @@ const Toolbar = ({ initialData, preview }: ToolbarProps) => {
     )}
     <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4">
       {!initialData.icon && !preview && (
-      <IconPicker asChild onChange={() => {}}>
+      <IconPicker asChild onChange={onIconSelect}>
         <Button
         className="text-muted-foreground text-xs"
         variant="outline"
