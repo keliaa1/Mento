f2c86ca8dd3d2d9bda2af6ae4b2dcad7a0daab42
Kelia Simbi <simbikelia@gmail.com>
2026-01-07 07:21:38 +0200

    feat: add buttons for changing and removing cover image in Cover component

--- components/Cover.tsx
+++ components/Cover.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import { ImageIcon, X } from "lucide-react";
 interface CoverImageProps{
     url?: string;
     preview?: boolean;
@@ -20,6 +22,29 @@ export const Cover = ({url, preview}: CoverImageProps)=>{
             unoptimized
             className="object-cover"
             />
+           )}
+           {url && !preview &&(
+            <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+                <Button
+                onClick={()=>{}}
+                variant="outline"
+                size="sm"
+                className="text-muted-foreground text-xs"
+                >
+                    <ImageIcon className="h-4 w-4 mr-2" />
+                    Change cover
+                </Button>
+
+                <Button
+                onClick={()=>{}}
+                variant="outline"
+                size="sm"
+                className="text-muted-foreground text-xs"
+                >
+                    <X className="h-4 w-4 mr-2" />
+                    Remove cover
+                </Button>
+            </div>
            )}
         </div>
     )
