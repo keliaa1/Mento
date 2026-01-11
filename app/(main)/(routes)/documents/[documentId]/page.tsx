5e1ba9f5ec4a99ce2dd70bc432215c64e99fd35e
Kelia Simbi <simbikelia@gmail.com>
2026-01-11 17:19:48 +0200

    fix: enhance loading state in DocumentIdPage with skeleton placeholders and add Cover.Skeleton component for detailed skeleton

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -6,6 +6,7 @@ import Toolbar from "@/components/toolbar";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
 import { Cover } from "@/components/Cover";
+import { Skeleton } from "@/components/ui/skeleton";
 const DocumentIdPage = () => {
   const params = useParams();
 
@@ -13,7 +14,20 @@ const DocumentIdPage = () => {
     documentId: params.documentId as Id<"documents">, // cast if TS complains
   });
 
-  if (document === undefined) return <div>Loading...</div>;
+  if (document === undefined) return(
+    <div>
+      <Cover.Skeleton />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+        <div className="space-y-4 pl-8 pt-4">
+          <Skeleton className="h-14 w-[50%]" />
+          <Skeleton className="h-4 w-[80%]" />
+          <Skeleton className="h-4 w-[40%]" />
+          <Skeleton className="h-4 w-[60%]" />
+        </div>
+
+      </div>
+    </div>
+  )
   if (document === null) return <div>Document not found</div>;
 
   return (
